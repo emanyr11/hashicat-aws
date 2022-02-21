28af1e9c7e76b463a0d9c79a8609a7f87a7f7271
Eman BHP <emmanuel.reid@bhp..com>
2022-02-21 06:18:24 +0000

    specify version

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,10 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
+  version = "2.8.0"
   bucket = "${var.prefix}"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
 }
